4bfbd23c1e52af8aa0f71004e47c3eb871756cce
Stefano Lanza <steflanz@gmail.com>
2025-12-26 13:09:30 +0100

    Fix

--- include/luaBind/result.h
+++ include/luaBind/result.h
@@ -4,6 +4,8 @@
 #ifdef __cpp_lib_expected
 #include <expected>
 
+namespace Typhoon::LuaBind {
+
 template <typename T, typename E>
 using Expected = std::expected<T, E>;
 
@@ -14,16 +16,23 @@ using Result = std::expected<void, const char*>;
 template <typename T>
 using ResultT = std::expected<T, const char*>;
 
+}
+
 #else
 
 #include <expected/include/tl/expected.hpp>
 
+namespace Typhoon::LuaBind {
+
 template <typename T, typename E>
 using Expected = tl::expected<T, E>;
+
 #define UNEXPECTED tl::make_unexpected
 using Result = tl::expected<void, const char*>;
 
 template <typename T>
 using ResultT = tl::expected<T, const char*>;
+
+}
 
 #endif
